4064d1c56d52c8feacec0bfd6f59ab7ba8896b3e
Umar Tariq <97828137+umar-tariq-1@users.noreply.github.com>
2025-06-02 10:26:43 +0500

    Delete frontend/gym_ease/lib/data/constants.dart

--- frontend/gym_ease/lib/data/constants.dart
+++
@@ -1,6 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const ZegoCloud_LiveStreaming_AppId = /* 188651988 */ /* 2031543951 */
-    851108131;
-const ZegoCloud_LiveStreaming_AppSign =
-    /* 'b3479feb87e98a80e810e8e62d55a38b97659df471f8b4690755ee9de472b5c7' */ /* 'ff18735bf42f75e20b91f59c72a6b1b2b210df710dab733593d74113214639c3' */ '4a2e87c0febee136aef0931ca4f93b8e16e93ed07b97e30f8f3a454c8fccb6b1';
